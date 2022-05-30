daec581bff476c96afffca02990669c3c073b753
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2022-05-30 15:46:34 +0700

    task 2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( // add browserRouter
   <React.StrictMode>
     <BrowserRouter>
       <App />
